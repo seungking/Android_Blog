2f35f5c9f4b46f4e8865ad0e6eb33a6959b3bd95
Seungki An <55059074+seungking@users.noreply.github.com>
2020-08-06 01:53:44 +0900

    20200806
    - part4 done

--- app/src/main/java/com/android_blog/MainActivity.java
+++ app/src/main/java/com/android_blog/MainActivity.java
@@ -2,7 +2,11 @@ package com.android_blog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //대기하고 로그인 확인인
+       Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+//                SharedPreferences userPref = getApplicationContext().getSharedPreferences("user", Context.MODE_PRIVATE);
+//                boolean isLoggedIn = userPref.getBoolean("isLoggedIn",false);
+//
+//                if (isLoggedIn){
+//                    startActivity(new Intent(MainActivity.this,HomeActivity.class));
+//                    finish();
+//                }
+//
+//                else {
+                    isFirstTime();
+//                }
+            }
+        },1500);
     }
+
+    private void isFirstTime() {
+        //맨처음 시작했는지 확인
+        SharedPreferences preferences = getApplication().getSharedPreferences("onBoard", Context.MODE_PRIVATE);
+        boolean isFirstTime = preferences.getBoolean("isFirstTime",true);
+
+        if (isFirstTime){
+            //처음
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("isFirstTime",false);
+            editor.apply();
+
+            //액티비티 이동
+            startActivity(new Intent(MainActivity.this,OnBoardActivity.class));
+            finish();
+        }
+        else{
+            //처음 아님
+            //인증 시작
+            startActivity(new Intent(MainActivity.this,AuthActivity.class));
+            finish();
+        }
+    }
+
 }
